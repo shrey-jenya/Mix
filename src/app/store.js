75c02118cdac88b36eb4a4e96769831fa7c1d3ac
Shrey <hardeepsinh.jenya@gmail.com>
2024-02-28 15:18:01 +0530

    redux-persist added

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import TodoSlice from "../features/TodoSlice";
+
+const persistConfig = {
+	key: "root",
+	storage: storage,
+	// blackList:['setSearchQuery']
+};
+
+const persistedReducer = persistReducer(persistConfig, TodoSlice);
 
 const store = configureStore({
 	reducer: {
-		todo: TodoSlice,
+		todo: persistedReducer,
 	},
 });
-export default store;
+
+const persistor = persistStore(store);
+
+export { store, persistor };
--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,17 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./app/store.js";
+import  {  persistor, store } from "./app/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
+			<PersistGate persistor={persistor} loading={null}>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</PersistGate>
 		</Provider>
 	</React.StrictMode>
 );
